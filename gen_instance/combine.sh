f5d41552603997f8b514f5baed76efd4a996db45
ConDug <115118796+ConDug@users.noreply.github.com>
2024-02-08 05:49:56 -0500

    mpcf

--- gen_instance/combine.sh
+++ gen_instance/combine.sh
@@ -2,17 +2,20 @@
 n=$1
 p=$2
 q=$3
-lower=$4
-upper=$5
-v=$6
-c=$7
+lower=${4:-0}
+upper=${5:-0}
+Edge_b=${6:-0} #upper bound on triangles per blue edge
+Edge_r=${7:-0} 
+mpcf=${8:-0}
+v=${9}
+c=${10}
 
 #Part of CNF file creation
 #v= total num var in cnf file
 #c= total num constraints in cnf file
 
-echo "p cnf ${v} ${c}" > ./temp_head_${n}_${p}_${q} #create cnf file and header
-cat ./temp_head_${n}_${p}_${q} ./constraints_temp_${n}_${p}_${q} > ./constraints_${n}_${p}_${q}_${lower}_${upper} #write constraints in temp_n_p_q to constraints
+echo "p cnf ${v} ${c}" > ./temp_head_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf} #create cnf file and header
+cat ./temp_head_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf} ./constraints_temp_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf} > ./constraints_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf} #write constraints in temp_n_p_q to constraints
 
-rm constraints_temp_${n}_${p}_${q}
-rm temp_head_${n}_${p}_${q}
+rm constraints_temp_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf}
+rm temp_head_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf}
