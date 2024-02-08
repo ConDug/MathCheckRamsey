5b6af3270088b1a13a99701a22094629a2c3ab51
ConDug <115118796+ConDug@users.noreply.github.com>
2024-02-08 05:49:14 -0500

    mpcf

--- caller_main.sh
+++ caller_main.sh
@@ -1,29 +1,53 @@
 #!/bin/bash
 #SBATCH --account=def-vganesh
-#SBATCH --time=10:00:00
+#SBATCH --time=02:00:00
 #SBATCH --mem-per-cpu=4G
 #SBATCH --nodes=1
 #SBATCH --constraint=broadwell
 
-while getopts "apsbm" opt
+while getopts "pmd:D:E:F:P" opt
 do
     case $opt in
         p) d="-p" ;;
         m) m="-m" ;;
+        d) lower=${OPTARG} ;; #lower bound on degree of blue vertices
+        D) upper=${OPTARG} ;; #upper bound on degree of blue vertices
+        E) Edge_b=${OPTARG} ;; #upper bound on triangles per blue edge
+        F) Edge_r=${OPTARG} ;; #upper bound on triangles per red edge
+        P) mpcf="-P" ;;
         *) echo "Invalid option: -$OPTARG. Only -p and -m are supported. Use -h or --help for help" >&2
            exit 1 ;;
-
-        esac
+    esac
+    
 done
 shift $((OPTIND-1))
 
-n=$1 #order
-p=${2}
-q=$3
-r=${4:-0} #num of var to eliminate during first cubing stage
-a=${5:-0} #amount of additional variables to remove for each cubing call
-lower=${6:-0}
-upper=${7:-0}
+if [[ ! -v lower ]]; then
+    lower=0
+fi
+if [[ ! -v upper ]]; then
+    upper=0
+fi
 
-module load python/3.10 
-./main.sh $d $n $p $q $r $a $lower $upper
+if [[ ! -v Edge_b ]]; then
+    Edge_b=0
+fi
+
+if [[ ! -v Edge_r ]]; then
+    Edge_r=0
+fi
+
+if [[ ! -v mpcf ]]; then
+    mpcf=0
+fi
+
+
+n=$1 #order
+p=$2
+q=$3
+t=${4:-100000} #conflicts for which to simplify each time CaDiCal is called, or % of variables to eliminate
+r=${5:-0} #number of variables to eliminate until the cubing terminates
+a=${6:-10}
+
+module load python/3.10
+./main.sh $d "-d" $lower "-D" $upper "-E" $Edge_b "-F" $Edge_r $mpcf $n $p $q $t $r $a $lower $upper
