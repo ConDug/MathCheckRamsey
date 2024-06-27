fd5cfa8dfad99894de10bc30d3174815a3d5bc5a
Daksh Sehgal <dsehgal34@gatech.edu>
2024-06-26 21:35:04 -0700

    formatting

--- gen_instance/generate.py
+++ gen_instance/generate.py
@@ -2,89 +2,154 @@ import sys
 import itertools
 from cubic import cubic
 import math
-import csv
 import subprocess
 from degree_constraints import generate_degree_clauses
 from triangle_constraints import generate_triangle_clauses
 from min_pclique_free import max_pclique_free
 from b_b_card import generate_edge_clauses
-#requires that the cnf file does not exist
-def generate(n, p, q,lower=0,upper=0, u_e_b=0, u_e_r=0,mpcf=0):
 
-    vertices=range(1,n+1)
-    edge_dict={}
-    edge_counter=0
 
-    for j in range(1, n+1):             #generating the edge variables
+# requires that the cnf file does not exist
+def generate(n, p, q, lower=0, upper=0, u_e_b=0, u_e_r=0, mpcf=0):
+
+    vertices = range(1, n + 1)
+    edge_dict = {}
+    edge_counter = 0
+
+    for j in range(1, n + 1):  # generating the edge variables
         for i in range(1, j):
             edge_counter += 1
-            edge_dict[(i,j)] = edge_counter
+            edge_dict[(i, j)] = edge_counter
 
-    for clique in itertools.combinations(vertices,p):
-        constraint=""
-        for j in itertools.combinations(clique,2):
-            constraint+=str(-edge_dict[j])+" "
-        with open(f"./constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}", 'a') as f: #p-cliques
+    for clique in itertools.combinations(vertices, p):
+        constraint = ""
+        for j in itertools.combinations(clique, 2):
+            constraint += str(-edge_dict[j]) + " "
+        with open(
+            f"./constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}",
+            "a",
+        ) as f:  # p-cliques
             f.write(constraint + "0" + "\n")
 
-    for clique in itertools.combinations(vertices,q):
-        constraint=""
-        for j in itertools.combinations(clique,2):
-            constraint+=str(edge_dict[j])+" "
-        with open(f"./constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}", 'a') as f: #q-cliques
-           f.write(constraint + "0" + "\n")
+    for clique in itertools.combinations(vertices, q):
+        constraint = ""
+        for j in itertools.combinations(clique, 2):
+            constraint += str(edge_dict[j]) + " "
+        with open(
+            f"./constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}",
+            "a",
+        ) as f:  # q-cliques
+            f.write(constraint + "0" + "\n")
 
+    count, clause_count = cubic(
+        n,
+        math.comb(n, 2),
+        f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}",
+    )  # write cubic constraints to file and count their total variables, and num_cubic constriants
+    # clause_count =0
+    # count=math.comb(n,2)
+    # print(count,clause_count)
 
-    count,clause_count= cubic(n, math.comb(n,2),f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}") # write cubic constraints to file and count their total variables, and num_cubic constriants
-    #clause_count =0
-    #count=math.comb(n,2)
-    #print(count,clause_count)
-    
-    if lower>0:
-        for i in range(1,n+1):
-            deg_count,deg_clause=generate_degree_clauses([edge_dict[key] for key in edge_dict if i in key],lower,upper,count,f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}")
+    if lower > 0:
+        for i in range(1, n + 1):
+            deg_count, deg_clause = generate_degree_clauses(
+                [edge_dict[key] for key in edge_dict if i in key],
+                lower,
+                upper,
+                count,
+                f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}",
+            )
             print(deg_count)
-            clause_count +=deg_clause
-            count=deg_count #+= built into generate_degree_clauses
+            clause_count += deg_clause
+            count = deg_count  # += built into generate_degree_clauses
 
-    if u_e_b>0:
-        print('blue triangle constraints')
-        for i in range(1,n*(n-1)//2+1):
-            X=set(range(1,n+1)) - set(list(edge_dict.keys())[list(edge_dict.values()).index(i)])#select all vertices except on i'th edge
-            edge_count,edge_clause=generate_triangle_clauses(X,u_e_b,count,f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}",colour='b')
-            #print('edges_blue',edge_count)
-            clause_count +=edge_clause
-            count=edge_count #+= built into generate_degree_clauses
+    if u_e_b > 0:
+        print("blue triangle constraints")
+        for i in range(1, n * (n - 1) // 2 + 1):
+            X = set(range(1, n + 1)) - set(
+                list(edge_dict.keys())[list(edge_dict.values()).index(i)]
+            )  # select all vertices except on i'th edge
+            edge_count, edge_clause = generate_triangle_clauses(
+                X,
+                u_e_b,
+                count,
+                f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}",
+                colour="b",
+            )
+            # print('edges_blue',edge_count)
+            clause_count += edge_clause
+            count = edge_count  # += built into generate_degree_clauses
 
-    if u_e_r>0:
-        print('red triangle constraints')
-        for i in range(1,n*(n-1)//2+1):
-            X=set(range(1,n+1)) - set(list(edge_dict.keys())[list(edge_dict.values()).index(i)])#select all vertiecs expect on i'th edge
-            edge_count,edge_clause=generate_triangle_clauses(X,u_e_r,count,f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}",colour='r')
-            #print('edges_red',edge_count)
-            clause_count +=edge_clause
-            count=edge_count #+= built into generate_degree_clauses
+    if u_e_r > 0:
+        print("red triangle constraints")
+        for i in range(1, n * (n - 1) // 2 + 1):
+            X = set(range(1, n + 1)) - set(
+                list(edge_dict.keys())[list(edge_dict.values()).index(i)]
+            )  # select all vertiecs expect on i'th edge
+            edge_count, edge_clause = generate_triangle_clauses(
+                X,
+                u_e_r,
+                count,
+                f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}",
+                colour="r",
+            )
+            # print('edges_red',edge_count)
+            clause_count += edge_clause
+            count = edge_count  # += built into generate_degree_clauses
 
-    if mpcf==0:
-        mtf_count,mtf_clause=max_pclique_free(n,p,count,f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}")
-        clause_count+=mtf_clause
-        count=mtf_count
-        MPCF='MPCF'
+    if mpcf == 0:
+        mtf_count, mtf_clause = max_pclique_free(
+            n,
+            p,
+            count,
+            f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}",
+        )
+        clause_count += mtf_clause
+        count = mtf_count
+        MPCF = "MPCF"
     else:
-        MPCF=0
-   
-    print('80 edges') 
-    #edge_count,edge_clause=generate_degree_clauses(list(range(1,math.comb(n,2)+1)),61,61,count,f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}")
-    edge_count,edge_clause=generate_edge_clauses(list(range(1,math.comb(n,2)+1)),80,80,count,f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}")
-    clause_count +=edge_clause
-    count=edge_count
+        MPCF = 0
 
-    count=str(count)
-    clause_count =str(clause_count+math.comb(n,p)+math.comb(n,q))
+    # edge_count,edge_clause=generate_degree_clauses(list(range(1,math.comb(n,2)+1)),61,61,count,f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}")
+    edge_count, edge_clause = generate_edge_clauses(
+        list(range(1, math.comb(n, 2) + 1)),
+        80,
+        80,
+        count,
+        f"constraints_temp_{n}_{p}_{q}_{lower}_{upper}_{u_e_b}_{u_e_r}_{mpcf}",
+    )
+    clause_count += edge_clause
+    count = edge_count
+
+    count = str(count)
+    clause_count = str(clause_count + math.comb(n, p) + math.comb(n, q))
     print(str(u_e_r), str(MPCF), count, clause_count)
-    proc1=subprocess.Popen(["./gen_instance/combine.sh",str(n), str(p), str(q), str(lower), str(upper), str(u_e_b), str(u_e_r), str(MPCF), count, clause_count]) # call a bash file to combine cubic constraints and 1st line of cnf file
+    proc1 = subprocess.Popen(
+        [
+            "./gen_instance/combine.sh",
+            str(n),
+            str(p),
+            str(q),
+            str(lower),
+            str(upper),
+            str(u_e_b),
+            str(u_e_r),
+            str(MPCF),
+            count,
+            clause_count,
+        ]
+    )  # call a bash file to combine cubic constraints and 1st line of cnf file
     proc1.wait()
 
 
 if __name__ == "__main__":
-    generate(int(sys.argv[1]), int(sys.argv[2]), int(sys.argv[3]),int(sys.argv[4]),int(sys.argv[5]),int(sys.argv[6]),int(sys.argv[7]),sys.argv[8])
+    generate(
+        int(sys.argv[1]),
+        int(sys.argv[2]),
+        int(sys.argv[3]),
+        int(sys.argv[4]),
+        int(sys.argv[5]),
+        int(sys.argv[6]),
+        int(sys.argv[7]),
+        sys.argv[8],
+    )
--- main.sh
+++ main.sh
@@ -28,24 +28,24 @@ Options:
     <nodes>: Number of nodes to submit to if using -l (default: 1)
 " && exit
 
-
-while getopts "nsld:D:E:F:P" opt
-do
+while getopts "nsld:D:E:F:P" opt; do
     case $opt in
-        n) solve_mode="no_cubing" ;;
-        s) solve_mode="sin_cubing" ;;
-        l) solve_mode="mul_cubing" ;;
-        d) lower=${OPTARG} ;; #lower bound on degree of blue vertices
-        D) upper=${OPTARG} ;; #upper bound on degree of blue vertices
-        E) Edge_b=${OPTARG} ;; #upper bound on blue triangles per blue edge
-        F) Edge_r=${OPTARG} ;; #upper bound on red triangles per red edge
-        P) mpcf="MPCF" ;;
-        *) echo "Invalid option: -$OPTARG. Only -p and -m are supported. Use -h or --help for help" >&2
-           exit 1 ;;
+    n) solve_mode="no_cubing" ;;
+    s) solve_mode="sin_cubing" ;;
+    l) solve_mode="mul_cubing" ;;
+    d) lower=${OPTARG} ;;  #lower bound on degree of blue vertices
+    D) upper=${OPTARG} ;;  #upper bound on degree of blue vertices
+    E) Edge_b=${OPTARG} ;; #upper bound on blue triangles per blue edge
+    F) Edge_r=${OPTARG} ;; #upper bound on red triangles per red edge
+    P) mpcf="MPCF" ;;
+    *)
+        echo "Invalid option: -$OPTARG. Only -p and -m are supported. Use -h or --help for help" >&2
+        exit 1
+        ;;
     esac
-    
+
 done
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 
 if [[ ! -v lower ]]; then
     lower=0
@@ -66,8 +66,7 @@ if [[ ! -v mpcf ]]; then
     mpcf=0
 fi
 #step 1: input parameters
-if [ -z "$1" ]
-then
+if [ -z "$1" ]; then
     echo "Need instance order (number of vertices) and number of simplification, use -h or --help for further instruction"
     exit
 fi
@@ -76,11 +75,10 @@ n=$1 #order
 p=$2
 q=$3
 t=${4:-100000} #conflicts for which to simplify each time CaDiCal is called, or % of variables to eliminate
-m=${5:-2} #Num of MCTS simulations. m=0 activate march
-d=${6:-v} #Cubing cutoff criteria, choose d(depth) as default #d, n, v
-dv=${7:-50} #By default cube to depth 5
-nodes=${8:-1} #Number of nodes to submit to if using -l
-
+m=${5:-2}      #Num of MCTS simulations. m=0 activate march
+d=${6:-v}      #Cubing cutoff criteria, choose d(depth) as default #d, n, v
+dv=${7:-50}    #By default cube to depth 5
+nodes=${8:-1}  #Number of nodes to submit to if using -l
 
 #step 2: setp up dependencies
 ./dependency-setup.sh
@@ -90,14 +88,12 @@ cnf="constraints_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf}"
 echo $di
 #step 3 and 4: generate pre-processed instance
 
-if [ -f ${cnf}_${t}_${m}_${d}_${dv}_${nodes}_final.simp.log ]
-then
+if [ -f ${cnf}_${t}_${m}_${d}_${dv}_${nodes}_final.simp.log ]; then
     echo "Instance with these parameters has already been solved."
     exit 0
 fi
 
-if [ -f ${cnf} ]
-then
+if [ -f ${cnf} ]; then
     echo "instance already generated"
     cp ${cnf} ${cnf}_${t}_${m}_${d}_${dv}_${nodes}
 else
@@ -110,57 +106,57 @@ echo $solve_mode
 cp ${cnf}_${t}_${m}_${d}_${dv}_${nodes} $di
 # Solve Based on Mode
 case $solve_mode in
-    "no_cubing")
-        echo "No cubing, just solve"
-        
-        echo "Simplifying $f for t conflicts using CaDiCaL+CAS"
-        ./simplification/simplify-by-conflicts.sh ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes} $n $t
+"no_cubing")
+    echo "No cubing, just solve"
 
-        echo "Solving $f using MapleSAT+CAS"
-        ./solve-verify.sh $n ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes}.simp
-        ;;
-    "sin_cubing")
-    	 echo "Simplifying $f for t conflicts using CaDiCaL+CAS"
-        ./simplification/simplify-by-conflicts.sh ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes} $n $t
-	mv ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes}.simp ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes}
-        echo "Cubing and solving in parallel on local machine"
-        python parallel-solve.py $n ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes} $m $d $dv
-        ;;
-    "mul_cubing")
-        echo "Simplifying $f for t conflicts using CaDiCaL+CAS"
-        ./simplification/simplify-by-conflicts.sh ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes} $n $t
-	mv ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes}.simp ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes}
-        echo "Cubing and solving in parallel on Compute Canada"
-        python parallel-solve.py $n ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes} $m $d $dv False
-        found_files=()
+    echo "Simplifying $f for t conflicts using CaDiCaL+CAS"
+    ./simplification/simplify-by-conflicts.sh ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes} $n $t
 
-        # Populate the array with the names of files found by the find command
+    echo "Solving $f using MapleSAT+CAS"
+    ./solve-verify.sh $n ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes}.simp
+    ;;
+"sin_cubing")
+    echo "Simplifying $f for t conflicts using CaDiCaL+CAS"
+    ./simplification/simplify-by-conflicts.sh ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes} $n $t
+    mv ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes}.simp ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes}
+    echo "Cubing and solving in parallel on local machine"
+    python parallel-solve.py $n ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes} $m $d $dv
+    ;;
+"mul_cubing")
+    echo "Simplifying $f for t conflicts using CaDiCaL+CAS"
+    ./simplification/simplify-by-conflicts.sh ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes} $n $t
+    mv ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes}.simp ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes}
+    echo "Cubing and solving in parallel on Compute Canada"
+    python parallel-solve.py $n ${di}/${cnf}_${t}_${m}_${d}_${dv}_${nodes} $m $d $dv False
+    found_files=()
 
-        while IFS= read -r -d $'\0' file; do
+    # Populate the array with the names of files found by the find command
+
+    while IFS= read -r -d $'\0' file; do
         found_files+=("$file")
-	#old
-	#done < <(find "${di}" -mindepth 1 ! -name '*.drat' ! -name '*.ext' ! -name '*.ext1' ! -name '*.simp1' ! -name '*.simplog' ! -name '*.cubes' -print0)
+        #old
+        #done < <(find "${di}" -mindepth 1 ! -name '*.drat' ! -name '*.ext' ! -name '*.ext1' ! -name '*.simp1' ! -name '*.simplog' ! -name '*.cubes' -print0)
         #done < <(find "${di}" -mindepth 1 -regex ".*\.\(11.cnf\|12.cnf\|21.cnf\|22.cnf\)$" -print0)
-	done < <(find "${di}" -mindepth 1 -name "*.cnf" -print0)
+    done < <(find "${di}" -mindepth 1 -name "*.cnf" -print0)
 
-        # Calculate the number of files to distribute names across and initialize counters
-        total_files=${#found_files[@]}
-        files_per_node=$(( (total_files + nodes - 1) / nodes )) # Ceiling division to evenly distribute
-        counter=0
-        file_counter=1
+    # Calculate the number of files to distribute names across and initialize counters
+    total_files=${#found_files[@]}
+    files_per_node=$(((total_files + nodes - 1) / nodes)) # Ceiling division to evenly distribute
+    counter=0
+    file_counter=1
 
-        # Check if there are files to distribute
-        if [ ${#found_files[@]} -eq 0 ]; then
-            echo "No files found to distribute."
-            exit 1
-        fi
+    # Check if there are files to distribute
+    if [ ${#found_files[@]} -eq 0 ]; then
+        echo "No files found to distribute."
+        exit 1
+    fi
 
-        # Create $node number of files and distribute the names of found files across them
-        for file_name in "${found_files[@]}"; do
-            # Determine the current output file to write to
-            output_file="${di}/node_${file_counter}.txt"
-            submit_file="${di}/node_${file_counter}.sh"
-            cat <<EOF > "$submit_file"
+    # Create $node number of files and distribute the names of found files across them
+    for file_name in "${found_files[@]}"; do
+        # Determine the current output file to write to
+        output_file="${di}/node_${file_counter}.txt"
+        submit_file="${di}/node_${file_counter}.sh"
+        cat <<EOF >"$submit_file"
 #!/bin/bash
 #SBATCH --account=def-vganesh
 #SBATCH --nodes=1
@@ -178,18 +174,17 @@ source ENV/bin/activate
 python parallel-solve.py $n $output_file $m $d $dv
 
 EOF
-            
-            # Write the current file name to the output file
-            echo "${file_name}.simp" >> "$output_file"
-            
-            # Update counters
-            ((counter++))
-            if [ "$counter" -ge "$files_per_node" ]; then
-                counter=0
-                ((file_counter++))
-            fi
-        done
 
+        # Write the current file name to the output file
+        echo "${file_name}.simp" >>"$output_file"
 
-        ;;
+        # Update counters
+        ((counter++))
+        if [ "$counter" -ge "$files_per_node" ]; then
+            counter=0
+            ((file_counter++))
+        fi
+    done
+
+    ;;
 esac
