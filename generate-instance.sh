e88092621eac177bd43b024ca7b4d56471be05c5
Brian Li <brianli@Brians-MacBook-Pro-2.local>
2024-09-07 22:16:00 -0400

    update generate-instance.sh

--- generate-instance.sh
+++ generate-instance.sh
@@ -1,31 +1,45 @@
 #!/bin/bash
 
-[ "$1" = "-h" -o "$1" = "--help" -o "$#" -lt 4 ] && echo "
+[ "$1" = "-h" -o "$1" = "--help" -o "$#" -lt 8 ] && echo "
 Description:
     Updated on 2023-01-11
-    This script call the python file generate.py in gen_instance to generate the SAT encoding for a Kochen Specker candidates. Such candidate satisfies the following condition:
+    This script calls the python file generate.py in gen_instance to generate the SAT encoding for Ramsey number candidates. Such candidates satisfy the following conditions:
     1. The graph is squarefree, hence does not contain C4 subgraph
     2. All vertices are part of a triangle
-    3. Graph is not 010-colorable (refer to the paper for definition)
-    4. Minimum degree of each vertex is 3
+    3. The graph avoids monochromatic cliques of specified sizes
+    4. Minimum degree of each vertex is specified
     5. We also applied the cubic isomorphism blocking clauses
 
 Usage:
-    ./generate-instance.sh n
+    ./generate-instance.sh n p q lower upper u_e_b u_e_r mpcf
 
 Options:
     <n>: the order of the instance/number of vertices in the graph
+    <p>: size of monochromatic clique to avoid in first color
+    <q>: size of monochromatic clique to avoid in second color
+    <lower>: lower bound for degree constraints
+    <upper>: upper bound for degree constraints
+    <u_e_b>: parameter for blue triangle constraints
+    <u_e_r>: parameter for red triangle constraints
+    <mpcf>: maximum p-clique free parameter (0 or 1)
+
+Example:
+    ./generate-instance.sh 18 4 4 8 9 1 1 1
     
 " && exit
 
-n=$1 #order
-p=${2} 
+n=$1
+p=$2
 q=$3
 lower=$4
 upper=$5
-if [ -f constraints_${n}_${p}_${q}_${lower}_${upper} ]
+u_e_b=$6
+u_e_r=$7
+mpcf=$8
+
+if [ -f constraints_${n}_${p}_${q}_${lower}_${upper}_${u_e_b}_${u_e_r}_${mpcf} ]
 then
     echo "instance already generated"
 else
-    python3 gen_instance/generate.py $n $p $q $lower $upper #generate the instance of order n
+    python3 gen_instance/generate.py $n $p $q $lower $upper $u_e_b $u_e_r $mpcf
 fi
