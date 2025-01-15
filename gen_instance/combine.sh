116cecbe4c77d33be333335eaa7a552b6450e671
BrianLi009 <brianlizhengyu@gmail.com>
2025-01-14 23:02:36 -0500

    add combine.sh

--- gen_instance/combine.sh
+++ gen_instance/combine.sh
@@ -2,20 +2,21 @@
 n=$1
 p=$2
 q=$3
-lower=${4:-0}
-upper=${5:-0}
-Edge_b=${6:-0} #upper bound on triangles per blue edge
-Edge_r=${7:-0} 
-mpcf=${8:-0}
-v=${9}
-c=${10}
+lower=$4
+upper=$5
+u_e_b=$6
+u_e_r=$7
+mpcf=$8
+count=$9
+clause_count=${10}
+temp_filename=${11}
+final_filename=${12}
 
-#Part of CNF file creation
-#v= total num var in cnf file
-#c= total num constraints in cnf file
+# Create the CNF header
+echo "p cnf $count $clause_count" > header
 
-echo "p cnf ${v} ${c}" > ./temp_head_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf} #create cnf file and header
-cat ./temp_head_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf} ./constraints_temp_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf} > ./constraints_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf} #write constraints in temp_n_p_q to constraints
+# Combine header with temp file into final file
+cat header "$temp_filename" > "$final_filename"
 
-rm constraints_temp_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf}
-rm temp_head_${n}_${p}_${q}_${lower}_${upper}_${Edge_b}_${Edge_r}_${mpcf}
+# Cleanup
+rm header "$temp_filename"
